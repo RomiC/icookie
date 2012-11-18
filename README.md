5eb1cad04d9fead3b6b5657ef30fa9f5e269985c
Roman Charugin <i@charugin.ru>
2012-11-18 18:36:48 +0400

    Fixed getting cookie value; Add description.

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-icookie
-=======
+# iCookie #
 
-micro-class for working with cookies on javascript
+Micro-class for working with cookies on javascript.
+
+## Methods ##
+
+*	**set(name, value, period)** – set cookie with name "name" with value "value" for period "period". "name" and "value" are strings. "period" could be date object of end date or string – humanity name of a period: "day", "week", "month", "year" or "forever".
+*	**get(name)** – return value of cookie or boolean false, if cookie "name" is not set or expired.
+*	**isSet(name)** – check if cookie "name" exists.
+*	**unset(name)** - unset cookie "name" (set cookie expired date in the past)
+
+## Samples ##
+
+```
+iCookie.set("foo", "bar", "week")	// Set cookie "foo" with "bar"-value for a week 
+
+console.log(iCookie.get("foo"))		// This will output "bar" in the console``
+
+if (iCookie.isSet("PHPSESSID"))		// Check if cookie "PHPSESSID" is set
+	iCookie.unset("PHPSESSID")		// and unset it
+```
--- icookie.js
+++ icookie.js
@@ -10,24 +10,24 @@ iCookie = {
 	 */
 	set: function(name, value, expires) {
 		if (typeof expires == "object")
-			expiresStr = expires.toLocaleString();
+			expiresStr = expires.toGMTString();
 		else if (typeof expires == "string") {
 			switch (expires) {
 				case "day":
-					expiresStr = (new Date(Date.now() + 86400000)).toLocaleString();
+					expiresStr = (new Date(Date.now() + 86400000)).toGMTString();
 					break;
 				case "week":
-					expiresStr = (new Date(Date.now() + 604800000)).toLocaleString();
+					expiresStr = (new Date(Date.now() + 604800000)).toGMTString();
 					break;
 				case "month":
-					expiresStr = (new Date((new Date).setMonth((new Date).getMonth() + 1))).toLocaleString();
+					expiresStr = (new Date((new Date).setMonth((new Date).getMonth() + 1))).toGMTString();
 					break;
 				case "year":
-					expiresStr = (new Date((new Date).setYear((new Date).getFullYear() + 1))).toLocaleString();
+					expiresStr = (new Date((new Date).setYear((new Date).getFullYear() + 1))).toGMTString();
 					break;
 			}
 		} else	// set cookie for a day, by default
-			expiresStr = (new Date(Date.now() + 86400000)).toLocaleString();
+			expiresStr = (new Date(Date.now() + 86400000)).toGMTString();
 
 		document.cookie = name + "=" + escape(value) + "; expires=" + expiresStr +"; path=/";
 	},
@@ -37,7 +37,7 @@ iCookie = {
 	 * @return string|boolean Cookie value, or false if it isn't set
 	 */
 	get: function(name) {
-		reg = new RegExp(name +"\=([^;]+)?;");
+		reg = new RegExp(name +"\=([^;]+)?(;|$)");
 		res = reg.exec(document.cookie);
 
 		if (res == null)
@@ -51,14 +51,14 @@ iCookie = {
 	 * @return boolean true — is set, false — no set
 	 */
 	isSet: function(name) {
-		reg = new RegExp(name +"\=([^;]+)?;");
+		reg = new RegExp(name +"\=([^;]+)?(;|$)");
 		return reg.test(document.cookie);
 	},
 	/**
 	 * Delete cookie
 	 * @param string name
 	 */
-	delete: function(name) {
+	unset: function(name) {
 		this.set(name, "", (new Date(5135072006)));
 	}	
 }
